f2e5c1df7e6761808d43e69fe36673083a794263
alexander1914 <alexander.oliveira99@gmail.com>
2025-10-14 23:19:32 +0100

    [Build CRUD REST APIs with MySQL] - I was creating a new project to review the JPA contents today

---
+++ springjpa-restfull-webservices/src/main/java/github/alexander1914/springjpa/restfull/SpringjpaRestfullWebservicesApplication.java
@@ -0,0 +1,13 @@
+package github.alexander1914.springjpa.restfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjpaRestfullWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjpaRestfullWebservicesApplication.class, args);
+	}
+
+}
